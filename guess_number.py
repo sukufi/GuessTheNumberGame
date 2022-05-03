f171cf65059c19487c391c8f6a373324d653323d
sukufi <60154684+sukufi@users.noreply.github.com>
2022-05-03 23:42:12 +0300

    higher,lower ile döngü eklendi

--- guess_number.py
+++ guess_number.py
@@ -1,16 +1,18 @@
 import random
 
-difficulty = int(input("Pick a difficulty level (1,2,3) to start the game"))
-guess = int(input(f"What is your guess 1 to {difficulty * 10}? "))
+difficulty = 10 * int(input("Pick a difficulty level (1,2,3) to start the game: ")) 
 
-def randomizer():
-    hardness = difficulty * 10
-    number = random.randint(1, hardness)
-    return number
+def main():
+    randomNumber = random.randint(1, difficulty)
+    print(randomNumber)
+    guess = ""
+    while guess != randomNumber:
+        guess = int(input(f"Guess the number! 1 to {difficulty}? "))
+        if guess > randomNumber:
+            print("Go more Lower!")
+        elif guess < randomNumber:
+            print("Go more Higher!")
+        else:
+            print(f"Congrats the number was {randomNumber}. You beat the game!")
 
-answer = randomizer()
-
-if guess == answer:
-    print(f"You win! {answer} is the true answer.")
-else:
-    print(f"You lose! {answer} was the true answer.")
+main()
