b106e894fe7d33fcbc68ba3b093cd1d8bfb20193
sukufi <60154684+sukufi@users.noreply.github.com>
2022-05-03 21:59:56 +0300

    code düzenlendi

--- guess_number.py
+++ guess_number.py
@@ -1,18 +1,16 @@
 import random
 
 difficulty = int(input("Pick a difficulty level (1,2,3) to start the game"))
+guess = int(input(f"What is your guess 1 to {difficulty * 10}? "))
 
-if difficulty == 1:
-    hardness = 10
-elif difficulty == 2:
-    hardness = 20
+def randomizer():
+    hardness = difficulty * 10
+    number = random.randint(1, hardness)
+    return number
+
+answer = randomizer()
+
+if guess == answer:
+    print(f"You win! {answer} is the true answer.")
 else:
-    hardness = 30
-
-number = random.randint(0,hardness)
-guess = int(input(f"What is your guess 1 to {hardness}? "))
-
-if guess == number:
-    print(f"You win! {number} is the true answer.")
-else:
-    print(f"You lose! {number} was the true answer.")
+    print(f"You lose! {answer} was the true answer.")
